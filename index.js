cab745a062af6aa88646a27907b37f9cbb70d6ca
nguyen tuan <nguyentuanspkt1607@gmail.com>
2023-05-14 01:46:00 +0700

    up

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 
-
 require('./config/environment')(app);
 require('./routes/routers')(app);
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+////
+
+git add .
+git commit -am "up"
+git push orgin main
+
+
+/// lệnh update srouce
+git pull origin main
